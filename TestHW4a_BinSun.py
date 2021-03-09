b423422676eb847cd5db4d444e338dbf3328efe1
bryansun96 <63320624+bryansun96@users.noreply.github.com>
2021-03-09 11:36:34 -0500

    Update TestHW4a_BinSun.py

--- TestHW4a_BinSun.py
+++ TestHW4a_BinSun.py
@@ -12,18 +12,18 @@ class TestHW4a(unittest.TestCase):
         """ test case for checking results of valid user name """
 
         repos = get_repo("richkempinski")
-        self.assertEqual(len(repos), 5)
-        self.assertEqual(repos[0], "hellogitworld")
-        self.assertEqual(repos[1], "helloworld")
-        self.assertEqual(repos[2], "Mocks")
-        self.assertEqual(repos[3], "Project1")
-        self.assertEqual(repos[4], "threads-of-life")
+        self.assertEqual(len(repos), 9)
+        self.assertEqual(repos[0], "csp")
+        self.assertEqual(repos[1], "hellogitworld")
+        self.assertEqual(repos[2], "helloworld")
+        self.assertEqual(repos[3], "Mocks")
+        self.assertEqual(repos[4], "Project1")
 
-        self.assertEqual(get_commits("richkempinski", repos[0]), 30)
-        self.assertEqual(get_commits("richkempinski", repos[1]), 6)
-        self.assertEqual(get_commits("richkempinski", repos[2]), 10)
-        self.assertEqual(get_commits("richkempinski", repos[3]), 2)
-        self.assertEqual(get_commits("richkempinski", repos[4]), 1)
+        self.assertEqual(get_commits("richkempinski", repos[0]), 2)
+        self.assertEqual(get_commits("richkempinski", repos[1]), 30)
+        self.assertEqual(get_commits("richkempinski", repos[2]), 6)
+        self.assertEqual(get_commits("richkempinski", repos[3]), 10)
+        self.assertEqual(get_commits("richkempinski", repos[4]), 2)
 
     def test_special_cases(self):
         """ Test cases for invalid input """
