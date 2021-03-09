5e496d8cb40b2dca04fee851b1bd8c9b3c590c0c
bryansun96 <63320624+bryansun96@users.noreply.github.com>
2021-03-08 19:56:18 -0500

    Update TestHW4a_BinSun.py

--- TestHW4a_BinSun.py
+++ TestHW4a_BinSun.py
@@ -1,35 +1,19 @@
 """ author: Bin Sun
 These are the test cases
+Test HW04a
 """
 import unittest
-from HW4a_BinSun import get_repo, get_commits
+from HW04a import result
 
-
-class TestHW4a(unittest.TestCase):
-    """ Test cases for HW4a """
-
-    def test_correct_result(self):
-        """ test case for checking results of valid user name """
-
-        repos = get_repo("richkempinski")
-        self.assertEqual(len(repos), 5)
-        self.assertEqual(repos[0], "hellogitworld")
-        self.assertEqual(repos[1], "helloworld")
-        self.assertEqual(repos[2], "Mocks")
-        self.assertEqual(repos[3], "Project1")
-        self.assertEqual(repos[4], "threads-of-life")
-
-        self.assertEqual(get_commits("richkempinski", repos[0]), 30)
-        self.assertEqual(get_commits("richkempinski", repos[1]), 6)
-        self.assertEqual(get_commits("richkempinski", repos[2]), 10)
-        self.assertEqual(get_commits("richkempinski", repos[3]), 2)
-        self.assertEqual(get_commits("richkempinski", repos[4]), 1)
-
-    def test_special_cases(self):
-        """ Test cases for invalid input """
-
-        self.assertEqual(get_repo("kkk"), [])
-
+class TestResult(unittest.TestCase):
+    """ test result() """
+    def testResult(self):
+        list1 = ['Repo: MLN Number of commits: 30',
+                 'Repo: MLNKV Number of commits: 14',
+                 'Repo: MLN_Toolkit Number of commits: 10']
+        self.assertEqual(result("momotech"), list1)
+        self.assertEqual(result("John567"), [])
 
 if __name__ == '__main__':
+    print('Running unit tests')
     unittest.main(exit=False, verbosity=2)
